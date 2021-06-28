5d53f1250421ced6a16474ecac02d5eb6e301261
dochi486 <93mskim@gmail.com>
2021-06-28 21:35:15 +0900

    버섯 죽는 거 모션 잘 수정

---
+++ Assets/Monster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public int hp = 10;
+
+    internal void OnDamage(int damage)
+    {
+        hp -= damage;
+
+            GetComponentInChildren<Animator>().Play("TakeHit");
+  
+        if(hp <= 0)
+        {
+            StartCoroutine(DieCo());
+            
+        }
+    }
+    public float dieDelay = 0.3f;
+    public float destroyDelay = 0.6f;
+    IEnumerator DieCo()
+    {
+        yield return new WaitForSeconds(dieDelay);
+        GetComponentInChildren<Animator>().Play("Death");
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerMeleeAttackCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeAttackCollider : MonoBehaviour
+{
+    public int damage = 1;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<Monster>()?.OnDamage(damage);
+
+        //Monster monster = collision.GetComponent<Monster>();
+        //if (monster == null)
+        //    return;
+        //monster.OnDamage(damage);
+    }
+}
